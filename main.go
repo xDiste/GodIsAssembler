0d2b60b6935c6c70bc688704f5acc0df8ed5323c
Alex Di Stefano <alex.distefano3@gmail.com>
2023-02-10 22:27:43 +0100

    small refactor

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func readELF(execPath string) *elf.File {
 func main() {
 	// Check if there is path of ELF file
 	args := os.Args
-    if len(args) == 1 || len(args) > 2 {
+    	if len(args) == 1 || len(args) > 2 {
 		fmt.Println("[*] Usage: patcher /path/of/ELF")
 		return
 	}
